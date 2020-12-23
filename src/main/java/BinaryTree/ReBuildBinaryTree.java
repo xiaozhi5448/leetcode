27541068b2067a2297722b6d762c984af1ea23a0
xiaozhi5448 <silenceaxx@gmail.com>
2020-12-23 22:39:27 +0800

    "重建二叉树（未完）"

---
+++ src/main/java/BinaryTree/ReBuildBinaryTree.java
@@ -0,0 +1,14 @@
+package BinaryTree;
+
+public class ReBuildBinaryTree {
+    private TreeNode buildTreeRecursionUtil(int[] preorder, int pre_start, int pre_end, int[] inorder, int in_start, int in_end){
+        if(pre_end - pre_start <= 1){
+            return new TreeNode(preorder[pre_start]);
+        }else{
+
+        }
+    }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+    }
+}
