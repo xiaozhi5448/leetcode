69c7df307867969502d270d4dfe9a366be719e3b
xiaozhi5448 <silenceaxx@gmail.com>
2020-12-28 12:11:20 +0800

    每日一题：买卖股票最佳时机

---
+++ src/main/java/dp/MaxProfitIV_188.java
@@ -0,0 +1,31 @@
+package dp;
+
+import java.util.Arrays;
+
+public class MaxProfitIV_188 {
+
+    public int maxProfit(int k, int[] prices) {
+        if(prices.length == 0){
+            return 0;
+        }
+        k = Math.min(k, prices.length /2);
+        int[][] buy = new int[prices.length][k+1];
+        int[][] sell = new int[prices.length][k+1];
+        buy[0][0] = -prices[0];
+        sell[0][0] = 0;
+        for(int i = 1; i < k+1;i++){
+            buy[0][i] = Integer.MIN_VALUE/2;
+            sell[0][i] = Integer.MIN_VALUE/2;
+        }
+
+        for(int day=1;day<prices.length;day++){
+            buy[day][0] = Math.max(sell[day-1][0] - prices[day], buy[day-1][0]);
+            for(int j = 1;j<=k;j++){
+                buy[day][j] = Math.max(sell[day-1][j] - prices[day], buy[day-1][j]);
+                sell[day][j]  = Math.max(buy[day-1][j-1] + prices[day], sell[day-1][j]);
+            }
+        }
+        int res = Arrays.stream(sell[prices.length-1]).max().getAsInt();
+        return res;
+    }
+}
