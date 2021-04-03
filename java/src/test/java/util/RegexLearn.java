e8284588509f893ad5ea5a6ce1fab281fd1d075f
yangzhile <silenceaxx@gmail.com>
2021-04-03 21:17:58 +0800

    fix

---
+++ java/src/test/java/util/RegexLearn.java
@@ -0,0 +1,24 @@
+package util;
+import java.util.regex.*;
+public class RegexLearn {
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile("(^[a-z]+)");
+        Matcher m = p.matcher("xiaozhi(13)");
+        System.out.println(m.matches());
+        System.out.println(m.find());
+        System.out.println(m.groupCount());
+        System.out.println(m.start(1));
+        System.out.println(m.end(1));
+        System.out.println(m.group());
+
+        p = Pattern.compile("\\((\\d+)\\)");
+        m = p.matcher("xiaozhi(18)");
+        System.out.println(m.matches());
+        System.out.println(m.find());
+        System.out.println(m.groupCount());
+        System.out.println(m.start());
+        System.out.println(m.end());
+        System.out.println(m.group());
+        System.out.println(m.group(1));
+    }
+}
