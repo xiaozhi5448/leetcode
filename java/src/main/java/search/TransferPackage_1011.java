c92c774593daeb1d57cd39a8d5cb8db24a234a45
yangzhile <silenceaxx@gmail.com>
2021-04-26 20:50:02 +0800

    排列组合

---
+++ java/src/main/java/search/TransferPackage_1011.java
@@ -0,0 +1,19 @@
+package search;
+
+/**
+ * 送带上的包裹必须在 D 天内从一个港口运送到另一个港口。
+ *
+ * 传送带上的第 i 个包裹的重量为 weights[i]。每一天，我们都会按给出重量的顺序往传送带上装载包裹。我们装载的重量不会超过船的最大运载重量。
+ *
+ * 返回能在 D 天内将传送带上的所有包裹送达的船的最低运载能力。
+ *
+ * 使用二分查找，查找可能的载重解空间，使得的D天之内可以运送完成
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/capacity-to-ship-packages-within-d-days
+ */
+
+public class TransferPackage_1011 {
+    public int shipWithinDays(int[] weights, int D) {
+        return 0;
+    }
+}
