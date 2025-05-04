df5f53c27a785c0e6042808222afec90b54a0964
xiaozhi5448 <1786614260@qq.com>
2025-05-05 01:10:51 +0800

    多米诺 双指针

--- python/source/cate_2pointer/longest_sub_array.py
+++ python/source/cate_2pointer/longest_sub_array.py
@@ -14,3 +14,55 @@ class Solution:
             ret = max(ret, right-left+1)
             right+=1
         return ret
+
+    def pushDominoes(self, dominoes: str) -> str:
+        if len(dominoes) < 2:
+            return dominoes
+        left, right = 0, 1
+        res = ['' for i in range(len(dominoes))]
+        while left < len(dominoes):
+            ele = dominoes[left]
+            while right < len(dominoes) and dominoes[right] == '.':
+                right += 1
+            if right == len(dominoes):
+                if ele == 'R':
+                    for i in range(left, right):
+                        res[i] = 'R'
+                    return ''.join(res)
+                elif ele == 'L':
+                    for i in range(left, right):
+                        res[i] = dominoes[i]
+            else:
+                if ele == '.':
+                    if dominoes[right] == 'L':
+                        for i in range(left, right):
+                            res[i] = 'L'
+                    elif dominoes[right] == 'R':
+                        for i in range(left, right):
+                            res[i] = dominoes[i]
+                elif ele == 'L':
+                    if dominoes[right] == 'L':
+                        for i in range(left, right):
+                            res[i] = 'L'
+                    elif dominoes[right] == 'R':
+                        for i in range(left, right):
+                            res[i] = dominoes[i]
+                elif ele == 'R':
+                    if dominoes[right] == 'R':
+                        for i in range(left, right):
+                            res[i] = 'R'
+                    elif dominoes[right] == 'L':
+                        i, j = left, right
+                        while i < j:
+                            res[i], res[j] = 'R', 'L'
+                            i += 1
+                            j -= 1
+                        if i == j:
+                            res[i] = dominoes[i]
+            left = right
+            right += 1
+        return ''.join(res)
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.pushDominoes(".L.R."))
